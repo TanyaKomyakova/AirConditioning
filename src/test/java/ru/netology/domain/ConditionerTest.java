90e6f037780f982f4a9abc3fa8ae6df862d62be2
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-06-05 10:44:45 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,66 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @org.junit.jupiter.api.Test
+    void TestIncreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMaxTemperature();
+        conditioner.getMinTemperature();
+        conditioner.setOn(true);
+        conditioner.setCurrentTemperature(30);
+        conditioner.increaseCurrentTemperature();
+        int expected = 30;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestdecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMaxTemperature();
+        conditioner.getMinTemperature();
+        conditioner.setOn(true);
+        conditioner.setCurrentTemperature(15);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 15;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void impossibleToGoBeyondTheMaximum() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMaxTemperature();
+        conditioner.getMinTemperature();
+        conditioner.setOn(true);
+        conditioner.setCurrentTemperature(30);
+        conditioner.setMaxTemperature(31);
+        int expected = 30;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void impossibleToGoBeyondTheMiniimum() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMaxTemperature();
+        conditioner.getMinTemperature();
+        conditioner.setOn(true);
+        conditioner.setCurrentTemperature(15);
+        conditioner.setMaxTemperature(14);
+        int expected = 15;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+}
+
