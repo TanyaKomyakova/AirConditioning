5a3fbdac17dd85ed0823a998dd8ac0d3691dd658
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-06-05 12:47:28 +0300

    added code

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -13,9 +13,9 @@ class ConditionerTest {
         conditioner.getMaxTemperature();
         conditioner.getMinTemperature();
         conditioner.setOn(true);
-        conditioner.setCurrentTemperature(30);
+        conditioner.setCurrentTemperature(25);
         conditioner.increaseCurrentTemperature();
-        int expected = 30;
+        int expected = 26;
         int actual = conditioner.getCurrentTemperature();
         assertEquals(expected, actual);
 
@@ -28,9 +28,9 @@ class ConditionerTest {
         conditioner.getMaxTemperature();
         conditioner.getMinTemperature();
         conditioner.setOn(true);
-        conditioner.setCurrentTemperature(15);
+        conditioner.setCurrentTemperature(18);
         conditioner.decreaseCurrentTemperature();
-        int expected = 15;
+        int expected = 17;
         int actual = conditioner.getCurrentTemperature();
         assertEquals(expected, actual);
     }
@@ -57,10 +57,37 @@ class ConditionerTest {
         conditioner.getMinTemperature();
         conditioner.setOn(true);
         conditioner.setCurrentTemperature(15);
-        conditioner.setMaxTemperature(14);
+        conditioner.setMinTemperature(14);
         int expected = 15;
         int actual = conditioner.getCurrentTemperature();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void increaseInMaximum(){
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMinTemperature();
+        conditioner.setOn(true);
+        int expected = 30;
+        conditioner.setCurrentTemperature(29);
+        conditioner.getMaxTemperature();
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(expected,conditioner.getMaxTemperature());
+    }
+
+    @Test
+    public void increaseInMinimum(){
+        Conditioner conditioner = new Conditioner();
+        conditioner.getName();
+        conditioner.getMaxTemperature();
+        conditioner.setOn(true);
+        int expected = 15;
+        conditioner.setCurrentTemperature(16);
+        conditioner.getMinTemperature();
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(expected,conditioner.getMinTemperature());
+    }
+
 }
 
